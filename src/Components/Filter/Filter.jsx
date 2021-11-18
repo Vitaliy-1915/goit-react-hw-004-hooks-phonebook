2ef940cd929d5dfa0bd1eeaec22872a70db57dce
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2021-11-19 00:30:03 +0200

    Update Filter.jsx

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -11,6 +11,7 @@ function Filter({ onChange, value }) {
         name="name"
         value={value}
         onChange={onChange}
+        autocomplete="off"
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
         required
